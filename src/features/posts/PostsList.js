4963a362c55ae5f2b215bdb8168879c677849bd2
Keith Frazier <keithers98@gmail.com>
2022-10-10 21:20:08 -0700

    added reactions to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
@@ -21,6 +22,7 @@ export const PostsList = () => {
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
+      <ReactionButtons post={post} />
     </article>
   ))
 
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -7,19 +7,42 @@ const initialState = [
     title: 'First Post!',
     content: 'Hello!',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
   {
     id: '2',
     title: 'Second Post!',
     content: 'More text',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
 ]
+
+// Notice that our action object just contains the minimum amount of
+// information needed to describe what happened. We know which post
+// we need to update, and which reaction name was clicked on.
+// We could have calculated the new reaction counter value and put
+// that in the action, but it's always better to keep the action
+// objects as small as possible, and do the state update calculations
+// in the reducer. This also means that reducers can contain as much
+// logic as necessary to calculate the new state.
+
+// When using Immer, you can either "mutate" an existing state object,
+// or return a new state value yourself, but not both at the same time.
+// See the Immer docs guides on Pitfalls and Returning New Data for
+// more details.
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
     postAdded: {
       reducer(state, action) {
         state.push(action.payload)
@@ -47,6 +70,6 @@ const postsSlice = createSlice({
   },
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
